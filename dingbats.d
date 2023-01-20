0aa540a122ac99148bffbb3eec16ac6c4ce6d1bc
monkyyy <crazymonkyyy@gmail.com>
2023-01-20 09:47:10 -0700

    some clean up and making sure that whats on github is whats on my machine from a month ago

--- dingbats.d
+++ dingbats.d
@@ -1,12 +1,12 @@
 #!dmd -run 
 import std;
-enum filecount=2;
+enum filecount=5;
 void main(){
 	"running dingbats".writeln;
 	foreach(i;1..filecount+1){
 		retry:
 		string s=executeShell("dmd -run source/example"~i.to!string).output;
-		string t=File("./target/"~i.to!string).byLineCopy.join('\n')~"\n";
+		string t=File("./.target/"~i.to!string).byLineCopy.join('\n')~"\n";
 		if(s==t){
 			writeln("test ",i,": success");
 		} else{
