24211b6c713d09b57f9acc5de7bb7786b2782b6a
monkyyy <crazymonkyyy@gmail.com>
2023-01-24 10:01:17 -0700

    first try at upgrades script

--- dingbats.d
+++ dingbats.d
@@ -1,9 +1,9 @@
 #!dmd -run 
 import std;
-enum filecount=7;
+enum filecounts=[17,20,23];
 void main(){
 	"running dingbats".writeln;
-	foreach(i;1..filecount+1){
+	foreach(i;1..filecounts[0]+1){//hello world era
 		retry:
 		string s=executeShell("dmd -run source/example"~i.to!string).output;
 		string t=File("./.target/"~i.to!string).byLineCopy.join('\n')~"\n";
@@ -14,6 +14,24 @@ void main(){
 			s.writeln;
 			"expected:".writeln;
 			t.writeln;
+			"press enter to retry".writeln;
+			readln;
+			goto retry;
+		}
+	}
+	foreach(i;filecounts[0]+1..filecounts[1]+1){//unix era
+		"example ".writeln(i," isnt actaully testable atm, when your happy with your solution hit enter");
+		readln;
+	}
+	foreach(i;filecounts[1]+1..filecounts[2]+1){//unittest era
+		retry:
+		auto shell=executeShell("dmd -run source/example"~i.to!string);
+		int e=shell.status;
+		if(e==0){
+			writeln("test ",i,": success");
+		} else {
+			writeln("test ",i,": fail");
+			shell.output.writeln;
 			"press enter to retry".writeln;
 			readln;
 			goto retry;
