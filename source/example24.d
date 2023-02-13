c917f0f9f9d827b9217f85c749ad0e06004548e9
monkyyy <crazymonkyyy@gmail.com>
2023-02-13 15:00:57 -0700

    forcing myself to do anything, example 24

---
+++ source/example24.d
@@ -0,0 +1,51 @@
+/*
+compile time is 2nd class
+
+d as a c style languge doesnt have great access to more fancy conpects, but 
+theres several hacky ways to do computation at compile time
+
+the magic words here are:
+
+enum, if a "value" needs to be stored compile time, slap a enum infront of its calculation
+for "ctfe", a formal covering of that topic is outside my apility. but 
+"compile time function execution".
+
+alias, if a type needs to be stored compile time define it with an alias, 
+I dont actaully understand whats its doing and am scepicle of anyone who
+would claim they do, but its a heavy concept thats doing allot 
+and will be explored later
+
+tl;dr THE MAGIC WORDS ARE ENUM AND ALIAS
+*/
+
+int calcsizeofarray(){
+	return 2+2;
+}
+//HINT: the solution is only editing these 3 lines
+compiletime int sizeofarray=calcsizeofarray;
+type mymathtype=int;
+type myarraytype=mymathtype[sizeofarray];
+
+
+int i;
+myarraytype gendata(){
+	myarraytype o;
+	foreach(ref e;o){
+		e=i++;
+	}
+	return o;
+}
+void writedata(myarraytype i){
+	import std;
+	foreach(e;i){
+		(e/2).writeln;//oh no the data type should be floats
+}}
+
+
+import std;
+void main(){
+	myarraytype foo=gendata;
+	myarraytype bar=gendata;
+	bar.writedata;
+	foo.writedata;
+}
